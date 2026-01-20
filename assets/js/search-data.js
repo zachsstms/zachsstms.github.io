c3fb24cd476089ffba1bb356edbcfbc3c6c066fe
zachsstms <78750482+zachsstms@users.noreply.github.com>
2026-01-20 09:33:55 +0000

    Deploying to gh-pages from @ zachsstms/zachsstms.github.io@0ddfa3e3ecc82690522dff6603ffcae2cd3830ab 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -455,13 +455,6 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/Resume-3.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
