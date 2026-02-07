8b84d690c5c9a558a13ad20c97242527be4bf453
zachsstms <78750482+zachsstms@users.noreply.github.com>
2026-02-07 23:56:16 +0000

    Deploying to gh-pages from @ zachsstms/zachsstms.github.io@a3c199e8725ac4c04a69ebb3fd4d41a2e3798883 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-blog",
+  },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
