5469a8ec1a9be4322b9f3e191b934036fbf16ebb
zachsstms <78750482+zachsstms@users.noreply.github.com>
2026-06-23 16:53:56 +0000

    Deploying to gh-pages from @ zachsstms/zachsstms.github.io@b0be4dcd8e3826d198df9b6c4394e769fe528bd0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,7 +38,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2025/firstpost/";
+            window.location.href = "/blog/2026/firstpost/";
           
         },
       },{id: "books-the-godfather",
